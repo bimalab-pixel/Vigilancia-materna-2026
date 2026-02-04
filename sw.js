eddc5b6eb49a8b0ea487ca9243b3e65977942bce
bimalab-pixel <orellanabimalab@gmail.com>
2026-02-04 10:16:23 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,27 @@
-const CACHE_NAME = 'vigilancia-materna-v1';
+const CACHE_NAME = 'vigilancia-materna-v2';
+
+// Archivos locales para Vigilancia Materna
 const ASSETS = [
   './',
   './index.html',
+  './calculadora.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  './html2pdf.bundle.min.js' // Asegúrate de incluir esta librería si la usas local
 ];
 
-// Instalación
-self.addEventListener('install', (e) => {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Instalando Sistema Vigilancia Materna...');
       return cache.addAll(ASSETS);
     })
   );
 });
 
-// Estrategia: Primero Cache, luego Red
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
